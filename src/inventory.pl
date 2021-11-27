ddb54fc9f767d71add1c13ad2bcd3b98a3be4886
Nadia Mareta <82803405+KorbanFidas2A@users.noreply.github.com>
2021-11-27 20:41:24 +0700

    update inventory, punya topan disimpen di local folder

--- src/inventory.pl
+++ src/inventory.pl
@@ -1,65 +1,39 @@
-item('hehe').
+:- dynamic(reservedSpace/1).
 
-:- dynamic(inventory/1).
+inventory(X) :- 
+    reservedSpace(X).
 
-inventory :- inventory(Inv), 
-    itemCount(Inv, Total),
-    (Total > 0 ->
-    write('Your inventory ('), write(Total), write('/100):'), nl
-    ; write('You don\'t have any items.')),
-    displayInventory(Inv).
+:- dynamic(storeditem/2).
 
-displayInventory([]) :- !.
-displayInventory([[Name, Count]|T]) :-
-    write(Count), write(' '), write(Name), nl, displayInventory(T).
+inInventory(X) :- 
+    storeditem(X,Y),
+    Y > 0.
 
-/* itemCount(X, Y) berarti dalam inventory X terdapat Y item */
-itemCount([], 0).
-itemCount([[_, Count]|T], Total) :-
-    itemCount(T, Temp),
-    Total is Count + Temp.
+saveItem(X) :- 
+    (
+        inventory(Reserved), Reserved < 100 ->
 
-/* drop(X, Y) membuang X sebanyak Y dari inventory jika ada */
-drop(Item) :- drop(Item, 1).
-drop(_, Count) :- Count =< 0, !, write('Item count must be more than 0'), fail.
-drop(Item, Count) :-
-    inventory(Inv),
-    (member([Item, CountInv], Inv) ->
-        (CountInv > Count ->
-            NewCount is CountInv - Count,
-            retract(inventory(Inv)),
-            delete(Inv, [Item, CountInv], TempInv),
-            append(TempInv, [[Item, NewCount]], NewInv),
-            asserta(inventory(NewInv))
-        ; CountInv =:= Count ->
-            retract(inventory(Inv)),
-            delete(Inv, [Item, CountInv], NewInv),
-            asserta(inventory(NewInv))
-        ;
-            write('You do not have that many item in your inventory')
-        )
-    ;
-        write('You do not have that item in your inventory')
+        (
+            storeditem(X, Y) ->(
+                retract(storeditem(X, Y)),
+                NewAmount is Y + 1,
+                asserta(storeditem(X, NewAmount)),
+                retract(reservedSpace(Reserved)),
+                NewAmount is Reserved + 1, 
+                asserta(reservedSpace(NewAmount))
+                
+            ); newItem(X, Reserved)
+
+        ); write('Your inventory is full. Throw some useless items. Or Sell? perhaps.'), nl
+
     ).
-     
-/* addItem(X, Y) menambahkan X sebanyak Y ke dalam inventory */
-addItem(_, Count) :- Count =< 0, !, write('Item count must be more than 0'), fail.
-addItem(Item, Count) :-
-    item(Item),
-    inventory(Inv),
-    itemCount(Inv, IC),
-    (Count + IC =< 100 ->
-        (member([Item, CountInv], Inv) ->
-            NewCount is CountInv + Count,
-            retract(inventory(Inv)),
-            delete(Inv, [Item, CountInv], TempInv),
-            append(TempInv, [[Item, NewCount]], NewInv),
-            asserta(inventory(NewInv ))
-        ;
-            retract(inventory(Inv)),
-            append(Inv, [[Item, Count]], NewInv),
-            asserta(inventory(NewInv))
-        )
-    ;
-        write('Failed to add item, inventory full')
-    ).
+
+newItem(Var, Used) :-
+    asserta(storeditem(Var, 1)),
+    retract(reservedSpace(Used)),
+    NewAmount is Used + 1,
+    asserta(reservedSpace(NewAmount)), 
+    write('You have added new item.'), nl.
+    
+itemCounter(Items, Counter) :-
+    storeditem(Items, Count).
