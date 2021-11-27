d4ebe45b5018b320e0c57c8752d120b079fafd2a
roastland <tfpruslanali@gmail.com>
2021-11-27 23:01:08 +0700

    sleep udah bisa nambah day

--- src/house.pl
+++ src/house.pl
@@ -1,7 +1,5 @@
-:- dynamic(diary/2).
-
 house :-
-    write('                 _ _'), nl,
+    write('                 _ _'), nl,  
     write('                ( Y )'), nl,
     write('                 \\ /  '), nl,
     write('                  \\          /^\\'), nl,
@@ -33,15 +31,42 @@ house :-
     write(' ".\'.\'." ." \'.". @/- - --- -- - -\\@ \'.".\'.".\'.".\'."'), nl,
     write(' ". \'\' ".".".\'.\'@/ - -- -- -- -- -\\@".\'..\'".\'."\'.\'.\''), nl,
     write('\'.".".\'\'.".\'\'."@/ -- --- --- -- - -\\@.".\'\'.".\'\'.".\'".'), nl, nl,
-    write('Welcome to your Home Sweet Home. After an exhausting day, you can rest here and wake up tomorrow with a full energy.'), nl,
-    write('If you are lucky, you will meet Snooze Fairy in your sleep and she could grants your wish to teleport to any places when you wake up.'), nl,
-    write(''), nl, nl,
+    write('***      Welcome to your Home Sweet Home.    ***'), nl,
+    write('After an exhausting day, you can rest here and wake up tomorrow with a full energy.'), nl,
+    write('If you are lucky, you will meet Snooze Fairy in your sleep and'), nl,
+    write('she could grants your wish to teleport to any places when you wake up.'), nl,
+    write('You can also write a diary about your journey today or read any diary you wrote in the past.'), nl, nl,
     write('**************************************'), nl,
-    write('*    So, what do you want to do?     *'), nl,
+    write('*    What do you want to do here?    *'), nl,
     write('**************************************'), nl,
     write('*        1. Sleep                    *'), nl,
     write('*        2. Write a Diary            *'), nl,
     write('*        3. Read a Diary             *'), nl,
     write('*        4. Exit House               *'), nl,
     write('**************************************'), nl,
-    %write('>'), read_integer(HouseMenu), nl,
+    write('>'), read_integer(HouseMenu), nl,
+    (HouseMenu = 1 ->
+            sleep
+        ;
+    !).
+
+:- dynamic(diary/2).
+:- dynamic(day/1).
+
+day(1).
+
+nextDay :-
+    retract(day(Dnow)),
+    Dnext is Dnow + 1,
+    asserta(day(Dnext)).
+
+sleep :-
+    write('Have a nice dream ^_^'), nl, nl,
+    write('...'), nl, nl,
+    nextDay, day(Today),
+    write('Good morning!'), nl,
+    write('Today is day '), write(Today), nl,
+    write('Good luck with your journey today!'), nl.
+
+%writeDiary :-
+
