3c6add7f4e427f433dd182d23269deea694d9770
Nadia Mareta <82803405+KorbanFidas2A@users.noreply.github.com>
2021-11-28 23:02:16 +0700

    fixing syntax

--- src/character.pl
+++ src/character.pl
@@ -13,21 +13,24 @@ createFisherman(X) :- asserta(job(X, fisherman)),
                       asserta(level(X, 1)),
                       asserta(level_farming(X, 1)),
                       asserta(exp_farming(X, 56)),
-                      asserta(level_fishing(X, 1)),
+                      asserta(level_fishing(X, 5)),
                       asserta(exp_fishing(X, 76)),
                       asserta(level_rancher(X, 1)),
                       asserta(exp_rancher(X, 56)),
                       asserta(stamina(X,0)),
                       asserta(exp(X, 0)),
                       asserta(gold(X, 1000)),
-                      asserta(fishingrod(X, 1, 200)),
-                      asserta(shovel(X, 1, 200)),
-                      asserta(bucket(X, 1, 200)).
+                      asserta(fishingrod(X, 5, 0)),
+                      asserta(shovel(X, 5, 200)),
+                      asserta(bucket(X, 5, 200)).
+baru :- 
+    asserta(reservedSpace(10)),  
+    asserta(storeditem(squid, 5)), asserta(storeditem(magic, 5)).
 
 /* Definisi job fisherman */
 
 :- dynamic(createFarmer/1).
-createFarmer(X) :-    asserta(job(X, fisherman)),
+createFarmer(X) :-    asserta(job(X, farmer)),
                       asserta(level(X, 1)),
                       asserta(level_farming(X, 1)),
                       asserta(exp_farming(X, 76)),
@@ -39,13 +42,13 @@ createFarmer(X) :-    asserta(job(X, fisherman)),
                       asserta(exp(X, 0)),
                       asserta(gold(X, 1000)),
                       asserta(fishingrod(X, 1, 200)),
-                      asserta(shovel(X, 1, 200)),
+                      asserta(shovel(X, 1, 0)),
                       asserta(bucket(X, 1, 200)).
 
 /* Definisi job fisherman */
 
 :- dynamic(createRancher/1).
-createRancher(X) :-   asserta(job(X, fisherman)),
+createRancher(X) :-   asserta(job(X, rancher)),
                       asserta(level(X, 1)),
                       asserta(level_farming(X, 1)),
                       asserta(exp_farming(X, 56)),
@@ -58,7 +61,7 @@ createRancher(X) :-   asserta(job(X, fisherman)),
                       asserta(gold(X, 1000)),
                       asserta(fishingrod(X, 1, 200)),
                       asserta(shovel(X, 1, 200)),
-                      asserta(bucket(X, 1, 200)).
+                      asserta(bucket(X, 1, 0)).
 
 /* Definisi Check Status */
 
@@ -77,15 +80,15 @@ checkStatus(Username) :-write(' _____  _                         _____ _        
                         write('Level          : '),
                         (level(Username, 50), write('MAX LEVEL!'), nl;
                         level(Username, Level), Level =\= 50, write(Level), nl),
-                        write('--- Farming ---'), n1,
+                        write('--- Farming ---'), nl,
                         write('Level Farmer  : '), level_farming(Username, LvlFarming), write(LvlFarming), nl,
-                        write('EXP Farmer    : '), exp_farming(Username, ExpFarming), write(ExpFarming), nl, n1,
-                        write('--- Fishing ---'), n1,
+                        write('EXP Farmer    : '), exp_farming(Username, ExpFarming), write(ExpFarming), nl, nl,
+                        write('--- Fishing ---'), nl,
                         write('Level Fisherman  : '), level_fishing(Username, LvlFishing), write(LvlFishing), nl,
-                        write('EXP Fisherman    : '), exp_fishing(Username, ExpFishing), write(ExpFishing), nl, n1,
-                        write('--- Raching ---'), n1,
+                        write('EXP Fisherman    : '), exp_fishing(Username, ExpFishing), write(ExpFishing), nl, nl,
+                        write('--- Raching ---'), nl,
                         write('Level Rancher  : '), level_rancher(Username, LvlRancher), write(LvlRancher), nl,
-                        write('EXP Rancher    : '), exp_rancher(Username, ExpRancher), write(ExpRancher), nl, n1,
+                        write('EXP Rancher    : '), exp_rancher(Username, ExpRancher), write(ExpRancher), nl, nl,
                         write('Stamina        : '), stamina(Username, Stamina), write(Stamina), nl,
                         write('EXP            : '), exp(Username, EXP), write(EXP), nl,
                         write('Gold           : '), gold(Username, Gold), write(Gold), nl, nl.
